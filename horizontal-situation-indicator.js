5ab1cb42b4b4308c512f9d94385aba1ea2ef241e
Frank Boës <fboes@3960.org>
2020-03-28 11:34:16 +0100

    New SVG with to NAV pointers

--- horizontal-situation-indicator.js
+++ horizontal-situation-indicator.js
@@ -4,65 +4,86 @@ template.innerHTML = `
 :host[hidden] { display: none; }
 :host svg { width: 100%; height: auto; }</style><svg width="256" height="256" version="1.1" viewBox="0 0 67.732 67.732" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g transform="translate(0 -229.27)">
- <rect id="base" y="229.27" width="67.733" height="67.733" style="paint-order:stroke fill markers"/>
- <circle cx="33.866" cy="263.13" r="17.395" fill="none" stroke="#fff" stroke-width=".74999" style="paint-order:stroke fill markers"/>
-</g>
-<g id="heading">
- <g id="nav1-course">
-  <circle cx="19.557" cy="33.866" r="1.1407" fill="none" stroke="#fff" stroke-width=".49999" style="paint-order:stroke fill markers"/>
-  <circle cx="48.179" cy="33.866" r="1.1407" fill="none" stroke="#fff" stroke-width=".49999" style="paint-order:stroke fill markers"/>
-  <path d="m33.866 5.5562-3.9687 6.3499 3.4395-1.3229v6.8791h1.0583v-6.8791l3.4395 1.3229zm-0.52916 12.435v6.6145l0.52916-1.0583 0.52916 1.0583v-6.6145zm0.52916 6.0853-2.6458 5.5562 2.1166-0.84645v20.955h1.0583v-20.955l2.1166 0.84645zm-0.52916 26.193v11.906h1.0583v-11.906z" fill="#f0f" style="paint-order:stroke fill markers"/>
-  <circle cx="26.597" cy="33.866" r="1.1407" fill="none" stroke="#fff" stroke-width=".49999" style="paint-order:stroke fill markers"/>
-  <circle cx="41.139" cy="33.866" r="1.1407" fill="none" stroke="#fff" stroke-width=".49999" style="paint-order:stroke fill markers"/>
- </g>
- <path id="heading-select" d="m32.014 1.3229h-1.5875v2.3812h6.8795l-4.26e-4 -2.3812h-1.5875l-1.8521 1.8521z" fill="#0ff"/>
- <g>
-  <use transform="rotate(-30 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <g fill="#000000" font-family="sans-serif" font-size="4.2333px" letter-spacing="0px" stroke-width=".26458" word-spacing="0px">
-   <text transform="rotate(-90)" x="-33.884247" y="13.858006" style="line-height:1.25" xml:space="preserve"><tspan x="-33.884247" y="13.858006" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">W</tspan></text>
-   <text transform="rotate(90)" x="33.849083" y="-53.875324" style="line-height:1.25" xml:space="preserve"><tspan x="33.849083" y="-53.875324" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">E</tspan></text>
-   <text transform="rotate(-60)" x="-12.413631" y="26.254101" style="line-height:1.25" xml:space="preserve"><tspan x="-12.413631" y="26.254101" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">30</tspan></text>
-   <text transform="rotate(120)" x="12.378468" y="-66.271416" style="line-height:1.25" xml:space="preserve"><tspan x="12.378468" y="-66.271416" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">12</tspan></text>
-   <text transform="rotate(-30)" x="12.378449" y="26.254034" style="line-height:1.25" xml:space="preserve"><tspan x="12.378449" y="26.254034" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">33</tspan></text>
-   <text transform="rotate(150)" x="-12.413613" y="-66.271355" style="line-height:1.25" xml:space="preserve"><tspan x="-12.413613" y="-66.271355" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">15</tspan></text>
-   <text x="33.848873" y="13.857946" style="line-height:1.25" xml:space="preserve"><tspan x="33.848873" y="13.857946" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">N</tspan></text>
-   <text transform="scale(-1)" x="-33.884041" y="-53.875267" style="line-height:1.25" xml:space="preserve"><tspan x="-33.884041" y="-53.875267" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">S</tspan></text>
-   <text transform="rotate(30)" x="46.244942" y="-7.6125116" style="line-height:1.25" xml:space="preserve"><tspan x="46.244942" y="-7.6125116" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">3</tspan></text>
-   <text transform="rotate(210)" x="-46.280106" y="-32.404808" style="line-height:1.25" xml:space="preserve"><tspan x="-46.280106" y="-32.404808" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">21</tspan></text>
-   <text transform="rotate(60)" x="46.244957" y="-32.404598" style="line-height:1.25" xml:space="preserve"><tspan x="46.244957" y="-32.404598" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">6</tspan></text>
-   <text transform="rotate(240)" x="-46.280121" y="-7.61272" style="line-height:1.25" xml:space="preserve"><tspan x="-46.280121" y="-7.61272" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">24</tspan></text>
-  </g>
-  <g id="g1294">
-   <path d="m33.866 4.2333v4.2333" fill="none" stroke="#fff" stroke-width=".49999"/>
-   <path d="m36.449 4.346-0.16142 1.845" fill="none" stroke="#fff" stroke-width=".49999"/>
-   <path d="m39.012 4.6834-0.55133 3.1267" fill="none" stroke="#fff" stroke-width=".49999"/>
-   <path d="m44.001 6.0201-1.0859 2.9835" fill="none" stroke="#fff" stroke-width=".49999"/>
-   <path d="m41.536 5.243-0.47935 1.789" fill="none" stroke="#fff" stroke-width=".49999"/>
-   <path d="m46.39 7.0096-0.78272 1.6785" fill="none" stroke="#fff" stroke-width=".49999"/>
-  </g>
-  <use transform="rotate(30 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(60 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(90 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(120 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(150 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(180 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(210 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(240 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(-90 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
-  <use transform="rotate(-60 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
- </g>
+<rect id="hsi-base" y="229.27" width="67.733" height="67.733" style="paint-order:stroke fill markers"/>
+<circle cx="33.866" cy="263.13" r="17.395" fill="none" stroke="#fff" stroke-width=".74999" style="paint-order:stroke fill markers"/>
 </g>
 <g>
- <path id="path968" d="m65.88 33.866h1.8521" fill="none" stroke="#fff" stroke-width=".49999"/>
- <path d="m36.247 38.629v-0.52916l-1.8521-1.5875 0.26458-2.1166 3.4395 1.0583v-1.0583l-3.4395-2.6458v-1.5875l-0.52916-1.5875h-0.26501 8.52e-4 -0.26501l-0.52916 1.5875v1.5875l-3.4395 2.6458v1.0583l3.4395-1.0583 0.26458 2.1166-1.8521 1.5875v0.52916l2.3812-0.52916z" fill="#fff"/>
- <path d="m29.633 0v1.3229h2.3812l1.8521 1.8521 1.8521-1.8521h2.3812v-1.3229z"/>
- <path d="m33.866 9.7895v-6.761" fill="none" stroke="#f60" stroke-width=".49999"/>
- <path d="m29.633 1.3229h2.3812l1.8521 1.8521 1.8521-1.8521h2.3812" fill="none" stroke="#fff" stroke-width=".26458px"/>
- <use id="use970" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#path968"/>
- <use id="use972" transform="rotate(-90 33.866 33.866)" width="100%" height="100%" xlink:href="#use970"/>
- <use id="use974" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#use972"/>
- <use id="use976" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#use974"/>
- <use transform="rotate(-90 33.866 33.866)" width="100%" height="100%" xlink:href="#use976"/>
+<g id="heading">
+<g id="nav2-course">
+<g id="nav2-deviation" fill="#0ff">
+<rect x="33.337" y="17.991" width="1.0583" height="31.75" style="paint-order:stroke fill markers"/>
+<path id="nav2-to" d="m31.22 29.633 2.6458-1.0583 2.6458 1.0583-2.6458-5.5562z" stroke="#000" stroke-width=".26458px"/>
+</g>
+<g fill="none" stroke="#fff" stroke-width=".49999">
+<circle cx="48.179" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="41.139" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="26.597" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="19.557" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+</g>
+<path d="m33.866 5.5562-3.9687 6.3499 3.4395-1.3229v6.8791h1.0583v-6.8791l3.4395 1.3229zm-0.52916 44.714v11.906h1.0583v-11.906z" fill="#0ff" style="paint-order:stroke fill markers"/>
+</g>
+<path id="nav1-bearing" d="m29.104 12.435 3.9687-1.3229v6.3499h-0.26458v-5.8208l-4.4979 1.3229 5.5562-8.4666v0.52916z" fill="#f0f"/>
+<g id="nav1-course">
+<g id="nav1-deviation" fill="#f0f">
+<rect x="33.337" y="17.991" width="1.0583" height="31.75" style="paint-order:stroke fill markers"/>
+<path id="nav1-to" d="m31.22 29.633 2.6458-1.0583 2.6458 1.0583-2.6458-5.5562z" stroke="#000" stroke-width=".26458px"/>
+</g>
+<g fill="none" stroke="#fff" stroke-width=".49999">
+<circle cx="48.179" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="41.139" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="26.597" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+<circle cx="19.557" cy="33.866" r="1.1407" style="paint-order:stroke fill markers"/>
+</g>
+<path id="nav1-course-pointer" d="m33.866 5.5562-3.9687 6.3499 3.4395-1.3229v6.8791h1.0583v-6.8791l3.4395 1.3229zm-0.52916 44.714v11.906h1.0583v-11.906z" fill="#f0f" style="paint-order:stroke fill markers"/>
+</g>
+<path id="heading-select" d="m32.014 1.3229h-1.5875v2.3812h6.8795l-4.26e-4 -2.3812h-1.5875l-1.8521 1.8521z" fill="#0ff"/>
+<g id="compass-rose">
+<use transform="rotate(-30 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<g fill="#000000" font-family="sans-serif" font-size="4.2333px" letter-spacing="0px" stroke-width=".26458" word-spacing="0px">
+<text transform="rotate(-90)" x="-33.884247" y="13.858006" style="line-height:1.25" xml:space="preserve"><tspan x="-33.884247" y="13.858006" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">W</tspan></text>
+<text transform="rotate(90)" x="33.849083" y="-53.875324" style="line-height:1.25" xml:space="preserve"><tspan x="33.849083" y="-53.875324" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">E</tspan></text>
+<text transform="rotate(-60)" x="-12.413631" y="26.254101" style="line-height:1.25" xml:space="preserve"><tspan x="-12.413631" y="26.254101" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">30</tspan></text>
+<text transform="rotate(120)" x="12.378468" y="-66.271416" style="line-height:1.25" xml:space="preserve"><tspan x="12.378468" y="-66.271416" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">12</tspan></text>
+<text transform="rotate(-30)" x="12.378449" y="26.254034" style="line-height:1.25" xml:space="preserve"><tspan x="12.378449" y="26.254034" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">33</tspan></text>
+<text transform="rotate(150)" x="-12.413613" y="-66.271355" style="line-height:1.25" xml:space="preserve"><tspan x="-12.413613" y="-66.271355" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">15</tspan></text>
+<text x="33.848873" y="13.857946" style="line-height:1.25" xml:space="preserve"><tspan x="33.848873" y="13.857946" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">N</tspan></text>
+<text transform="scale(-1)" x="-33.884041" y="-53.875267" style="line-height:1.25" xml:space="preserve"><tspan x="-33.884041" y="-53.875267" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">S</tspan></text>
+<text transform="rotate(30)" x="46.244942" y="-7.6125116" style="line-height:1.25" xml:space="preserve"><tspan x="46.244942" y="-7.6125116" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">3</tspan></text>
+<text transform="rotate(210)" x="-46.280106" y="-32.404808" style="line-height:1.25" xml:space="preserve"><tspan x="-46.280106" y="-32.404808" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">21</tspan></text>
+<text transform="rotate(60)" x="46.244957" y="-32.404598" style="line-height:1.25" xml:space="preserve"><tspan x="46.244957" y="-32.404598" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">6</tspan></text>
+<text transform="rotate(240)" x="-46.280121" y="-7.61272" style="line-height:1.25" xml:space="preserve"><tspan x="-46.280121" y="-7.61272" fill="#ffffff" font-family="Arial" font-size="4.9388px" font-weight="bold" stroke-width=".26458" text-align="center" text-anchor="middle">24</tspan></text>
+</g>
+<g id="g1294">
+<path d="m33.866 4.2333v4.2333" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m36.449 4.346-0.16142 1.845" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m39.012 4.6834-0.55133 3.1267" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m44.001 6.0201-1.0859 2.9835" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m41.536 5.243-0.47935 1.789" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m46.39 7.0096-0.78272 1.6785" fill="none" stroke="#fff" stroke-width=".49999"/>
+</g>
+<use transform="rotate(30 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(60 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(90 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(120 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(150 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(180 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(210 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(240 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(-90 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+<use transform="rotate(-60 33.867 33.866)" width="100%" height="100%" xlink:href="#g1294"/>
+</g>
+</g>
+</g>
+<g>
+<path id="path968" d="m65.88 33.866h1.8521" fill="none" stroke="#fff" stroke-width=".49999"/>
+<path d="m36.247 38.629v-0.52916l-1.8521-1.5875 0.26458-2.1166 3.4395 1.0583v-1.0583l-3.4395-2.6458v-1.5875l-0.52916-1.5875h-0.26501 8.52e-4 -0.26501l-0.52916 1.5875v1.5875l-3.4395 2.6458v1.0583l3.4395-1.0583 0.26458 2.1166-1.8521 1.5875v0.52916l2.3812-0.52916z" fill="#fff"/>
+<path d="m29.633 0v1.3229h2.3812l1.8521 1.8521 1.8521-1.8521h2.3812v-1.3229z"/>
+<path d="m29.633 1.3229h2.3812l1.8521 1.8521 1.8521-1.8521h2.3812" fill="none" stroke="#fff" stroke-width=".26458px"/>
+<use id="use970" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#path968"/>
+<use id="use972" transform="rotate(-90 33.866 33.866)" width="100%" height="100%" xlink:href="#use970"/>
+<use id="use974" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#use972"/>
+<use id="use976" transform="rotate(-45 33.866 33.866)" width="100%" height="100%" xlink:href="#use974"/>
+<use transform="rotate(-90 33.866 33.866)" width="100%" height="100%" xlink:href="#use976"/>
 </g>
 </svg>`;
 
@@ -117,10 +138,10 @@ class HorizontalSituationIndicator extends HTMLElement {
             if (degreeAttributes.indexOf(attrName) > -1) {
               attrValue = this._limitDeg(attrValue);
             }
+            this.setAttribute(attrName, attrValue);
             if (matches[2] === 'course' || matches[2] === 'bearing') {
               this._calculateDeviation(matches[1]);
             }
-            this.setAttribute(attrName, attrValue);
           }
           else {
             this.removeAttribute(attrName);
@@ -160,10 +181,14 @@ class HorizontalSituationIndicator extends HTMLElement {
 
     switch (matches[2]) {
       case 'to':
-        // switch el
+        el.setAttribute('transform','rotate(' + [this[attrName] > 0 ? 0 : 180 , this._svg.centerX, this._svg.centerY].join(' ') + ')');
         break;
       case 'deviation':
-        // move el
+        let value = this[attrName];
+        if (value < -90) { value += 180; value *= -1; }
+        else if (value > +90) { value -= 180; value *= -1; }
+        value = Math.max(-10, Math.min(10, value)) * -1.45;
+        el.setAttribute('transform','translate(' + value + ' 0)');
         break;
       default:
         el.setAttribute('transform','rotate(' + [this[attrName], this._svg.centerX, this._svg.centerY].join(' ') + ')');
@@ -175,14 +200,6 @@ class HorizontalSituationIndicator extends HTMLElement {
   }
 
   disconnectedCallback() {
-  }
-
-  _upgradeProperty(prop) {
-    if (this.hasOwnProperty(prop)) {
-      let value = this[prop];
-      delete this[prop];
-      this[prop] = value;
-    }
   }
 
   _getStructuredAttributeName(attrName) {
